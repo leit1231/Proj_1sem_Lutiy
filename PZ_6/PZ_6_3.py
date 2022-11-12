ad35cf9b9d6cbc72a85bafbbeab96bba905b42ad
leit1231 <gitaraigor@yandex.ru>
2022-11-12 12:56:07 +0300

    initial commit

---
+++ PZ_6/PZ_6_3.py
@@ -0,0 +1,27 @@
+# Дан список размера N. Осуществить циклический сдвиг элементов списка вправо на
+# одну позицию (при этом A1 перейдет в A2, A2 — в A3,..., AN — в A1).
+from random import randint  #импортирование модуля рандомных чисел
+while True:     #обработка исключений
+    try:
+        def shift(lst, steps):
+            if steps < 0:
+                steps = abs(steps)
+                for i in range(steps):
+                    lst.append(lst.pop(0))
+            else:
+                for i in range(steps):
+                    lst.insert(0, lst.pop())
+
+
+        N = randint(0, 10)
+        A = [randint(1, 10) for i in range(N)]
+        print(A)
+
+        shift(A, -1)
+        print(A)
+
+        shift(A, 3)
+        print(A)
+        break
+    except:
+        print('Ошибка!')
