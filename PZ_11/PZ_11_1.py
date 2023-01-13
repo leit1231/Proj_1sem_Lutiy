9f8443b4e9f8599acedfd9a20c7fcb497abab0f8
leit1231 <gitaraigor@yandex.ru>
2023-01-13 19:03:35 +0300

    initial commit

---
+++ PZ_11/PZ_11_1.py
@@ -0,0 +1,42 @@
+# Средствами языка Python сформировать текстовый файл (.txt), содержащий
+# последовательность из целых положительных и отрицательных чисел. Сформировать
+# новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую
+# обработку элементов:
+# Исходные данные:
+# Количество элементов:
+# Индекс первого минимального элемента:
+# Умножаем все элементы на минимальный элемент:
+list = ['-3 18 8 -4 54 9']
+file_1 = open('data_1.txt', 'w')
+file_1.writelines(list)
+file_1.close()
+
+file_2 = open('data_2.txt', 'w')
+file_2.write('Исходные данные: ')
+file_2.write('\n')
+file_2.writelines(list)
+file_2.close()
+
+file_3 = open('data_1.txt')
+k = file_3.read()
+k = k.split()
+for i in range(len(k)):
+ k[i] = int(k[i])
+file_3.close()
+
+mi=min(k)
+print(mi)
+minimum = (k.index(mi))
+
+data = []
+f = open('data_1.txt', 'r+')
+s = (f.read())
+a = s.split(',')
+print(a)
+f.close()
+# prois = [x*mi for x in list]
+
+f4 = open('data_2.txt', 'a') # открываем файл для дозаписи
+f4.write('\n')
+print('Количество элементов:', len(k),'\n' 'Индекс первого минимального элемента:', minimum, '\n' 'Умножаем все элементы на минимальный элемент:', a,file=f4)
+f4.close()
