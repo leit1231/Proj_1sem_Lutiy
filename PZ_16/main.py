8727378736157fcdc400d29c60f7da6739b9f2d1
leit1231 <gitaraigor@yandex.ru>
2023-04-21 17:23:53 +0300

    initial commit

---
+++ PZ_16/main.py
@@ -0,0 +1,7 @@
+from figures import circle_perimeter, circle_area, triangle_area, triangle_perimeter, square_perimeter, square_area
+circle_perimeter()
+circle_area()
+triangle_perimeter()
+triangle_area()
+square_area()
+square_perimeter()
